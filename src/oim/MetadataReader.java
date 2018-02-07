bd34170a5a05ac394f24042b36d9806e588652aa
Ali İhsan ŞAHİN <aliihsan.sahin@kaleyazilim.com.tr>
2018-02-07 17:21:01 +0300

    --- fix.

--- src/oim/MetadataReader.java
+++ src/oim/MetadataReader.java
@@ -12,7 +12,6 @@ import java.io.IOException;
 import java.util.Date;
 import java.util.HashMap;
 
-
 public class MetadataReader {    
     
     private String _imageName;
--- src/oim/OIM.java
+++ src/oim/OIM.java
@@ -1,8 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package oim;
 
 public class OIM {
@@ -12,7 +8,7 @@ public class OIM {
 
     public static void main(String[] args) {        
         
-        // readMetadata();
+        //mdr.readAndAssignMetadata(filename).forEach((key, value) -> System.out.println("--- " + key + " : " + value));
         mdr.readAndAssignMetadata(filename);
     }
 }
