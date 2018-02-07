a9d8257a3084fb0f388a19667c3f10e93ad08418
Ali İhsan ŞAHİN <aliihsan.sahin@kaleyazilim.com.tr>
2018-02-07 17:08:08 +0300

    --- Metadata reader moved to another class.

---
+++ src/oim/MetadataReader.java
@@ -0,0 +1,100 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package oim;
+
+import com.drew.imaging.ImageMetadataReader;
+import com.drew.imaging.ImageProcessingException;
+import com.drew.metadata.Directory;
+import com.drew.metadata.Metadata;
+import com.drew.metadata.Tag;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+import static oim.OIM.filename;
+
+/**
+ *
+ * @author aliihsan.sahin
+ */
+public class MetadataReader {    
+    
+    private String _imageName;
+    private Date _date;
+    private String _GPSLatitudeRef;
+    private String _GPSLatitude;
+    private String _GPSLongitudeRef;
+    private String _GPSLongitude;
+            
+    private Metadata metadata;
+    private String imageName;
+    private HashMap<String, HashMap<String, String>> outerMap = new HashMap<>();
+    private HashMap<String, String> innerMap = new HashMap<>();
+    
+    // default constructor.
+    public MetadataReader(){
+    
+    }
+    
+    // get images from specified directory.
+    private static File[] getAllFiles(String path) {
+        
+        File folder = new File(path);
+        File[] listOfFiles = folder.listFiles();
+        return listOfFiles;
+    }    
+    
+    // read metadata for each image and add them to a HashMap of HashMap.
+    public HashMap<String, HashMap<String, String>> readAndAssignMetadata(String path) {
+        
+        for(File file : getAllFiles(path)) {
+            
+            imageName = file.getName();
+            
+            try {
+                
+                metadata = ImageMetadataReader.readMetadata(file);
+        
+                for (Directory directory : metadata.getDirectories()) {
+
+                    for (Tag tag : directory.getTags()) {
+
+                        if (tag.getTagName().equalsIgnoreCase("Date/Time Original") 
+                            || tag.getTagName().equalsIgnoreCase("GPS Latitude Ref")
+                            || tag.getTagName().equalsIgnoreCase("GPS Latitude")
+                            || tag.getTagName().equalsIgnoreCase("GPS Longitude Ref")
+                            || tag.getTagName().equalsIgnoreCase("GPS Longitude"))
+                        {
+                            
+                            innerMap.put(tag.getTagName(), tag.getDescription());
+                        }
+                    }
+
+                    for (String error : directory.getErrors()) {
+
+                        System.err.println("ERROR: " + error);
+                    }            
+                }
+                
+                outerMap.put(imageName, innerMap);
+            }
+            
+            catch (ImageProcessingException | IOException e) { print(e); }
+        }
+                
+        outerMap.forEach((key, value) -> System.out.println("--- " + key + " : " + value));
+        
+        return outerMap;
+    }
+    
+    // print error while reading metadata of an image.
+    private static void print(Exception exception) {
+        
+        System.err.println("EXCEPTION: " + exception);
+    }
+}
--- src/oim/OIM.java
+++ src/oim/OIM.java
@@ -5,84 +5,14 @@
  */
 package oim;
 
-import com.drew.imaging.ImageMetadataReader;
-import com.drew.imaging.ImageProcessingException;
-import com.drew.metadata.Directory;
-import com.drew.metadata.Metadata;
-import com.drew.metadata.Tag;
-
-import java.io.File;
-import java.io.IOException;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.Map;
-
 public class OIM {
     
-    static String filename = "C:\\workspace\\fotoToSort";
-    static final String ANSI_RED = "\u001B[31m";
+    static String filename = "D:\\fotoToSort";
+    static MetadataReader mdr = new MetadataReader();
 
     public static void main(String[] args) {        
         
-        readMetadata();
-    }
-    
-    private static File[] getAllFiles(String path) {
-        
-        File folder = new File(path);
-        File[] listOfFiles = folder.listFiles();
-        return listOfFiles;
-    }
-    
-    private static void readMetadata() {
-        
-        for(File file : getAllFiles(filename)) {
-            
-            try {
-                
-                Metadata metadata = ImageMetadataReader.readMetadata(file);
-                print(metadata, "--- OUTPUT SEPARATOR FOR EACH IMAGE: " , file.getName() + "\n");
-            }
-            
-            catch (ImageProcessingException | IOException e) { print(e); }
-        }
-    }
-    
-    private static Map<String, String> print(Metadata metadata, String method, String myFile ) { 
-        
-        Map<String, String> theThings = new HashMap<>();
-        System.out.println();
-        System.out.println(ANSI_RED + "================================================================================");
-        System.out.print(method);
-        System.out.println(ANSI_RED + "================================================================================");
-        System.out.println();
-        
-        for (Directory directory : metadata.getDirectories()) {
-            
-            for (Tag tag : directory.getTags()) {
-                
-                if (tag.getTagName().equalsIgnoreCase("Date/Time Original") 
-                    || tag.getTagName().equalsIgnoreCase("GPS Latitude Ref")
-                    || tag.getTagName().equalsIgnoreCase("GPS Latitude")
-                    || tag.getTagName().equalsIgnoreCase("GPS Longitude Ref")
-                    || tag.getTagName().equalsIgnoreCase("GPS Longitude"))
-                {
-                    theThings.put(tag.getTagName(), tag.getDescription());
-                }
-            }
-            
-            for (String error : directory.getErrors()) {
-                
-                System.err.println("ERROR: " + error);
-            }            
-        }
-        theThings.forEach((key, value) -> System.out.println(key + " : " + value));
-        return theThings;
-    }
-
-    private static void print(Exception exception) {
-        
-        System.err.println("EXCEPTION: " + exception);
+        // readMetadata();
+        mdr.readAndAssignMetadata(filename);
     }
 }
